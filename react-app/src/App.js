50a851e53f823337552607d41f9619f23132537f
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-16 19:03:43 +0900

    [add]子コンポーネントの中の要素の利用

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,84 +1,61 @@
-import React, { Component } from "react";
-import Rect from "./Rect";
-import "./App.css";
+import React, { Component } from 'react';
+import Rect from './Rect';
+import './App.css';
+
 
 class App extends Component {
-  data = [
-    "This is list sample.",
-    "これはリストのサンプルです。",
-    "配列をリストに変換します。",
-  ];
+  input = '';
+
 
   msgStyle = {
-    fontSize: "20pt",
-    color: "#900",
-    margin: "20px 0px",
+    fontSize:"20pt",
+    color:"#900",
+    margin:"20px 0px",
     padding: "5px",
-  };
+  }
+  inputStyle = {
+    fontSize:"12pt",
+    padding:"5px"
+  }
 
-  constructor(props) {
+
+  constructor(props){
     super(props);
     this.state = {
-      list: this.data,
+      message:'type your name:'
     };
+    this.doChange = this.doChange.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
   }
 
-  render() {
-    return (
-      <div>
-        <h1> React </h1>
-        <h2 style={this.msgStyle}> show list. </h2>{" "}
-        <List title="サンプル・リスト" data={this.data} />{" "}
-      </div>
-    );
+
+  doChange(event) {
+    this.input = event.target.value;
   }
+
+
+  doSubmit(event) {
+    this.setState({
+      message: 'Hello, ' + this.input + '!!'
+    });
+    event.preventDefault();
+  }
+  render(){
+    return <div>
+      <h1>React</h1>
+      <h2>{this.state.message}</h2>
+      <form onSubmit={this.doSubmit}>
+        <label>
+          <span style={this.inputStyle}></span>Message:
+          <input type="text" style={this.inputStyle} onChange={this.doChange} />
+        </label>
+        <input type="submit" style={this.inputStyle} value="Click" />
+      </form>
+    </div>;
+  }
+
+
 }
 
-class List extends Component {
-  number = 1;
-
-  title = {
-    fontSize: "20pt",
-    fontWeight: "bold",
-    color: "blue",
-  };
-
-  render() {
-    let data = this.props.data;
-    return (
-      <div>
-        <p style={this.title}> {this.props.title} </p>{" "}
-        <ul>
-          {" "}
-          {data.map((item) => (
-            <Item number={this.number++} value={item} key={this.number} />
-          ))}{" "}
-        </ul>{" "}
-      </div>
-    );
-  }
-}
-
-class Item extends Component {
-  li = {
-    listStyleType: "square",
-    fontSize: "16pt",
-    color: "#06",
-    margin: "0px",
-    padding: "0px",
-  };
-  num = {
-    fontWeight: "bold",
-    color: "red",
-  };
-
-  render() {
-    return (
-      <li style={this.li}>
-        <span style={this.num}> [{this.props.number}] </span> {this.props.value}{" "}
-      </li>
-    );
-  }
-}
 
 export default App;
