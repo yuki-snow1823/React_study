1b7d450b0a00f11d954188770aa90dab23cbb794
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-16 13:23:11 +0900

    [add]プロジェクトとしてコンポーネントの分割

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super();
+    this.title = props.title;
+    this.message = props.message;
+  }
+  render() {
+    return (
+      <div>
+        <h1> {this.title} </h1> <p> {this.message} </p>{" "}
+      </div>
+    );
+  }
 }
 
 export default App;
