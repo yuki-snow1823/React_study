8456af16a3ea0e65a27eaafb422df659060d267f
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-17 20:07:18 +0900

    [add]メモアプリ：復習 switch文で検索時と通常時に表示を分岐

--- react-app/src/memo/Memo.js
+++ react-app/src/memo/Memo.js
@@ -4,16 +4,18 @@ import { connect } from "react-redux";
 import Item from "./Item";
 
 class Memo extends Component {
+  // コンポーネントも表示によって変える
   render() {
     let data;
     let n = 0;
-    switch (this.props.mode) {
-      case "default":
+    switch (this.props.mode) { //に、関してケースを分けるってこと
+      case "default": // それがdefaultなら
         data = this.props.data.map((value) => (
           <Item key={value.message} value={value} index={n++} />
         ));
         break;
-
+      
+      // 検索時は表示を変える
       case "find":
         data = this.props.fdata.map((value) => (
           <Item key={value.message} value={value} index={n++} />
