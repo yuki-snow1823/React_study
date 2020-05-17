4cf57de231cd1438111f11e0cf7447212c1bef7a
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-17 16:01:16 +0900

    [add]メモアプリ：index.jsの復習、storeの呼び出し確認

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
+
+// MemoStoreは指定した場所から読み込んでいる、というよりもstoreをメモストアって名前で呼び出している
+// 呼び出されるのがmemostoreって名前のわけではない
 import MemoStore from "./memo/Store";
 
 // 表示をレンダリング
