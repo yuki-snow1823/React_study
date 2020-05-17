31f97e2e79afe5654b0fe590602e22366cee8e00
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-17 14:48:14 +0900

    [add]メモアプリの作成：indexとappjs

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import "./App.css";
-
-// ステートのマッピング
-// 関連づけみたいな感じなのでは
-// 関数名は関係なく、stateの何を共有するかかける。
-// 全部きたら、本来のpropsの名前と被る可能性がある
-function mappingState(state) {
-  return state;
-}
+import Memo from "./memo/Memo";
+import AddForm from "./memo/AddForm";
+import FindForm from "./memo/FindForm";
+import DelForm from "./memo/DelForm";
 
 // Appコンポーネント
 class App extends Component {
+  td = {
+    width: "250px",
+  };
+
   constructor(props) {
     super(props);
   }
@@ -19,69 +19,28 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1> Redux </h1>
-        <Message />
-        <Button />
+        <h1> Memo </h1> <AddForm />
+        <hr />
+        <table>
+          {" "}
+          <tbody>
+            {" "}
+            <tr>
+              <td style={this.td}>
+                {" "}
+                <FindForm />{" "}
+              </td>{" "}
+              <td style={this.td}>
+                {" "}
+                <DelForm />{" "}
+              </td>{" "}
+            </tr>
+          </tbody>{" "}
+        </table>{" "}
+        <Memo />
       </div>
     );
   }
 }
-// ストアのコネクト
-App = connect()(App);
 
-// メッセージ表示のコンポーネント
-class Message extends Component {
-  style = {
-    fontSize: "20pt",
-    padding: "20px 5px",
-  };
-
-  render() {
-    return (
-      <p style={this.style}>
-        {" "}
-        {this.props.message}: {this.props.counter}{" "}
-      </p>
-    );
-  }
-}
-// ストアのコネクト
-Message = connect(mappingState)(Message);
-
-//
-class Button extends Component {
-  style = {
-    fontSize: "16pt",
-    padding: "5px 10px",
-  };
-
-  constructor(props) {
-    super(props);
-    this.doAction = this.doAction.bind(this);
-  }
-
-  // ボタンクリックでディスパッチを実行
-  doAction(e) {
-    if (e.shiftKey) {
-      this.props.dispatch({
-        type: "DECREMENT",
-      });
-    } else {
-      this.props.dispatch({
-        type: "INCREMENT",
-      });
-    }
-  }
-
-  render() {
-    return (
-      <button style={this.style} onClick={this.doAction}>
-        click{" "}
-      </button>
-    );
-  }
-}
-// ストアのコネクト
-Button = connect()(Button);
-
-export default App;
+export default connect()(App);
