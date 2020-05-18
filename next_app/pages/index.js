ea7cb1ae35046d0ecc411885987c65b8744fbf1b
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-18 20:00:30 +0900

    [add]index.jsでJSXの利用

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,5 +1,20 @@
+const h1 = {
+  fontSize: "72pt",
+  fontWeight: "bold",
+  textAlign: "right",
+  letterSpacing: "-8px",
+  color: "#f0f0f0",
+  margin: "-40px 0px",
+};
+
+const p = {
+  margin: "0px",
+  color: "#666",
+  fontSize: "16pt",
+};
+
 export default () => (
   <div>
-    <h1> Next.js </h1> <div> Welcome to next.js! </div>{" "}
+    <h1 style={h1}> Next.js </h1> <p style={p}> Welcome to next.js! </p>{" "}
   </div>
 );
