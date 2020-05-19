5a4a5985223c6996a0949fc7163ae6a3d3f98463
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-19 22:13:18 +0900

    [fix]classに関するバグ(classNameと書けばOK:予約語とぶつかる)

--- practice_react/src/index.js
+++ practice_react/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="投稿ページです！"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
