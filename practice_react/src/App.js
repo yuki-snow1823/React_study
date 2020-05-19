11638dc66a951cf5fd570216cae39d1b73179f87
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-19 21:43:33 +0900

    [add]タイトルのprops化

--- practice_react/src/App.js
+++ practice_react/src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.title = props.title;
+  }
   render() {
     return (
       <div>
-        <h1 class="index-title">投稿ページ</h1>
+        <h1 class="index-title">{this.title}</h1>
         <p>ここに愚痴を投稿します。</p>
         <input class="main-form" type="text"></input>
         <div class="button-wrapper">
