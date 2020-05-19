1b6b8f89bdd278fd92fe959459b7c0c59c7d4653
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-19 17:28:10 +0900

    [add]自作練習アプリ トップページの表示

--- practice_react/src/App.js
+++ practice_react/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 class="index-title">投稿ページ</h1>
+        <p>ここに愚痴を投稿します。</p>
+        <input class="main-form" type="text"></input>
+        <div class="button-wrapper">
+          <button class="submit-button">投稿</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
