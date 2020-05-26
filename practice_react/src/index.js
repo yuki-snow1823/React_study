a0ec391248fa1690b7575352b27787d9c783837b
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-26 10:51:45 +0900

    [add]store/reducer/providerの設定

--- practice_react/src/index.js
+++ practice_react/src/index.js
@@ -3,12 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// 追加
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+
+// ステートの値
+let state_value = {
+  counter: 0,
+  message: "こんにちは",
+};
+
+// レデューサー
+function counter(state = state_value, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1,
+        message: "増やしました。",
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1,
+        message: "減らしました。",
+      };
+    default:
+      return state;
+  }
+}
+// ストアを作成
+let store = createStore(counter);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App title="投稿ページです！"/>
+    {/* Provider(ストアを渡すもの)追加 */}
+    <Provider store={store}> 
+      <App title="投稿ページです！" />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
