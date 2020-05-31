808e6a7a762b0a4fc0ead4de61a8a8b202747178
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-31 20:19:34 +0900

    [add]index.jsの追加

--- my-tutorial/src/index.js
+++ my-tutorial/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+//  3つのコンポーネント
 class Square extends React.Component {
   render() {
     return <button className="square">{/* TODO */}</button>;
