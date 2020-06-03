48b80637be0055d9cfff3c8ca9f0172e5c4b9249
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-03 21:40:47 +0900

    [add]動きの出力

--- my-tutorial/src/index.js
+++ my-tutorial/src/index.js
@@ -51,8 +51,11 @@ class Board extends React.Component {
   // }
 
   handleClick(i) {
-    const squares = this.state.squares.slice();
+    // const squares = this.state.squares.slice();
     // squares[i] = "X";
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return; // これのおかげで、勝者が出るとゲームがもう終わってしまう。押してもreturnされるから。
     }
@@ -135,6 +138,15 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = "Winner: " + winner;
@@ -150,8 +162,8 @@ class Game extends React.Component {
           />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
